7a826ac23963d410d147bf23e35ca54ad5403e2f
irfanfiriezky <irfanfiriezkybahri@gmail.com>
2021-05-02 15:07:09 +0700

    Menambahkan fle index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       <meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width">
+       <title>Coba git remote</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <p>Anjay</p>
+    </body>
+</html>
